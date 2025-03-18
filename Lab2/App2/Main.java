6f36fd4f7efa797319646c5a9d33f9dce9445d7f
Mihai Mureșan <mihaimuresan592003@gmail.com>
2025-03-18 19:56:51 +0200

    add sequence diagram for Lab2 App2

--- Lab2/App2/Main.java
+++ Lab2/App2/Main.java
@@ -3,9 +3,11 @@ package Lab2.App2;
 public class Main {
   public static void main(String[] args) {
     ProfilerView view = new ProfilerView(threadCount);
+
     for (int i = 0; i < threadCount; i++) {
       ProfilerModel model = new ProfilerModel(i, processorLoad);
       model.start();
+
       new ProfilerController(model, view);
     }
   }
--- Lab2/App2/ProfilerView.java
+++ Lab2/App2/ProfilerView.java
@@ -9,26 +9,32 @@ import javax.swing.JProgressBar;
 
 @SuppressWarnings("deprecation")
 public class ProfilerView extends JFrame implements Observer {
-  public void update(Observable observable, Object arg) {
-    ProfilerModel model = (ProfilerModel) observable;
-    int id = model.getId();
-    int progress = model.getProgress();
-    bars.get(id).setValue(progress);
-  }
-
   public ProfilerView(int threadCount) {
     setLayout(null);
     setSize(530, 330);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
+
     init(threadCount);
+
     this.setVisible(true);
+  }
+
+  public void update(Observable observable, Object arg) {
+    ProfilerModel model = (ProfilerModel) observable;
+
+    int id = model.getId();
+    int progress = model.getProgress();
+
+    bars.get(id).setValue(progress);
   }
 
   private void init(int n) {
     for (int i = 0; i < n; i++) {
       JProgressBar progressBar = new JProgressBar();
+
       progressBar.setMaximum(1000);
       progressBar.setBounds(50, (i + 1) * 30, 350, 20);
+
       this.add(progressBar);
       this.bars.add(progressBar);
     }
